926e4996e688b8b9e7e85bc88d308e76a75dc8b0
Jiyin Zhang <zhangjiyin2000@gmail.com>
2026-06-24 16:39:29 -0700

    Initial commit

---
+++ src/main/java/com/bfs/projectstory/restapipracticefour/RestApiPracticeFourApplication.java
@@ -0,0 +1,13 @@
+package com.bfs.projectstory.restapipracticefour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiPracticeFourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiPracticeFourApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bfs/projectstory/restapipracticefour/RestApiPracticeFourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfs.projectstory.restapipracticefour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiPracticeFourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
